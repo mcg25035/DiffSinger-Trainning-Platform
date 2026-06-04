e9b03356ed65662126a88ad4b7a0c879ef1ed9e5
CodingBear <mcg25035@gmail.com>
2026-06-05 02:34:42 +0800

    fix: build error

--- frontend/src/components/label-editor/PhonemeEditPanel.tsx
+++ frontend/src/components/label-editor/PhonemeEditPanel.tsx
@@ -70,7 +70,7 @@ export function PhonemeEditPanel({
         }}
         onFocus={(e) => focusAndSelectAll(e.target, 0)}
       />
-      <button onClick={onUpdate} className="phoneme-edit__btn phoneme-edit__btn--ok">OK</button>
+      <button onClick={() => onUpdate()} className="phoneme-edit__btn phoneme-edit__btn--ok">OK</button>
       <button onClick={onPlay} className="phoneme-edit__btn phoneme-edit__btn--play">PLAY</button>
       <button onClick={onDelete} className="phoneme-edit__btn phoneme-edit__btn--del">DEL</button>
       <div className="phoneme-edit__divider" />
